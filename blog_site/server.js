f7d6f1b7648cfc944511ee2d9a267903aea66598
Samuel Warner <samuelrwarner1@gmail.com>
2016-06-16 18:59:03 -0400

    change server.js

--- blog_site/server.js
+++ blog_site/server.js
@@ -1,19 +1,11 @@
-//Lets require/import the HTTP module
-var http = require('http');
+var express = require('express');
+var app = express();
+app.use(express.static('Blog_Site'));
+app.get('/', function (req, res) {
+  res.send('home.html');
+});
 
-//Lets define a port we want to listen to
-const PORT=8080; 
-
-//We need a function which handles requests and send response
-function handleRequest(request, response){
-    response.end('It Works!! Path Hit: ' + request.url);
-}
-
-//Create a server
-var server = http.createServer(handleRequest);
-
-//Lets start our server
-server.listen(PORT, function(){
-    //Callback triggered when server is successfully listening. Hurray!
-    console.log("Server listening on: http://localhost:%s", PORT);
+var port = 8080;
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port + '!');
 });
