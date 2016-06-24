3ab873ebceb8f1cfab9042b7df911cd84db4dfd1
Samuel Warner <samuelrwarner1@gmail.com>
2016-06-24 15:16:49 -0400

    made db as a module export

--- Blog_Site/db.js
+++ Blog_Site/db.js
@@ -1,8 +1,31 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+module.exports = function(app){
+  var mongoose = require('mongoose');
+  mongoose.connect('mongodb://localhost/test');
+  var db = mongoose.connection;
+  db.on('error', console.error.bind(console, 'connection error:'));
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("we're connected!");
-});
+
+  db.once('open', function() {
+    console.log("~~~~~~we're connected to mangoDB!~~~~~~~");
+  	var schema = new mongoose.Schema({name: String, tasks: [] });
+  	var List = mongoose.model('List', schema);
+
+  //if there are no lists makes the default toDo and done lists
+  	List.find({}, function(err, lists){
+  		if(err) throw err;
+  		if (lists.length === 0) {
+  			var toDo = new List({ listName:"toDo", tasks:['click to move to the finished list!'] });
+  			var done = new List({ listName:"done", tasks:['click the X to delete me!'] });
+  			var saveList = function(listName){
+  				listName.save(function(err){ if(err) throw err;
+  					console.log(listName + " : saved!")
+  				});
+  			}
+  			saveList(toDo);
+  			saveList(done);
+  		}
+  		console.log(lists);
+  	});
+
+  });
+}
--- blog_site/server.js
+++ blog_site/server.js
@@ -6,39 +6,11 @@ var bodyParser = require('body-parser');
 var fs = require('fs')
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({ extended: true }));
-var routes = require('./routes.js');
-	routes(app);
+var routes = require('./routes.js');  routes(app);
+var database = require('./db.js');	database(app);
 var listData = require('./listData.js');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-	var schema = new mongoose.Schema({name: String, tasks: [] });
-	var List = mongoose.model('List', schema);
-
-//if there are no lists makes the default toDo and done lists
-	List.find({}, function(err, lists){
-		if(err) throw err;
-		if (lists.length === 0) {
-
-			var toDo = new List({ listName:"toDo", tasks:['click to move to the finished list!'] });
-			var done = new List({ listName:"done", tasks:['click the X to delete me!'] });
-			var saveList = function(listName){
-				listName.save(function(err){ if(err) throw err;
-					console.log(listName + " : saved!")
-				});
-			}
-			saveList(toDo);
-			saveList(done);
-		}
-		console.log(lists);
-	});
-
-  console.log("we're connected to mangoDB!");
-});
 
 app.get('/data', function (req, res) {
 	fs.readFile('data.txt', 'utf8', function (err, data) {
