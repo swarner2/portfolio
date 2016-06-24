2cec6e976c093a92c5f66d6356909cb1b204f061
Samuel Warner <samuelrwarner1@gmail.com>
2016-06-24 14:51:25 -0400

    autocreate toDo and done lists in mangoose

--- blog_site/server.js
+++ blog_site/server.js
@@ -16,21 +16,28 @@ mongoose.connect('mongodb://localhost/test');
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function() {
-	var schema = new mongoose.Schema({ tasks: [] });
+	var schema = new mongoose.Schema({name: String, tasks: [] });
 	var List = mongoose.model('List', schema);
-  var toDo = new List({
-		tasks	: [1,2,3]
-	});
-	toDo.save(function(err){
-		if (err) throw err;
-		console.log('toDo saved!')
-	});
 
+//if there are no lists makes the default toDo and done lists
 	List.find({}, function(err, lists){
 		if(err) throw err;
+		if (lists.length === 0) {
+
+			var toDo = new List({ listName:"toDo", tasks:['click to move to the finished list!'] });
+			var done = new List({ listName:"done", tasks:['click the X to delete me!'] });
+			var saveList = function(listName){
+				listName.save(function(err){ if(err) throw err;
+					console.log(listName + " : saved!")
+				});
+			}
+			saveList(toDo);
+			saveList(done);
+		}
 		console.log(lists);
 	});
-  console.log("we're connected!");
+
+  console.log("we're connected to mangoDB!");
 });
 
 app.get('/data', function (req, res) {
