518f92d585685df8597f1a331b28687757fe1212
Samuel Warner <samuelrwarner1@gmail.com>
2016-06-27 12:57:29 -0400

    added portfolio page

--- Blog_Site/routes.js
+++ Blog_Site/routes.js
@@ -3,18 +3,21 @@ module.exports = function(app){
 	//folder structure
 	var currPath =  path.join(__dirname) ;
 	var toDo = '/toDo'
+	var portfolio = '/portfolio/framework'
 	var scripts = '/javascript'
 	var styles = '/styles'
 	var	routes = {
-		'/' 					: '/home.html',
-		'/toDo' 		  : toDo + '/toDo.html',
-		'/toDoJs' 		: toDo + scripts + '/toDo.js',
-		'/draggableJs': toDo + scripts + '/draggable.js',
-		'/clientLists': toDo + scripts + '/clientLists.js',
-		'/onLoad'			: toDo + scripts + '/onLoad.js',
-		'/stylesheet'	: toDo + styles + '/stylesheet.css',
-		'/responsive' : toDo + styles + '/responsive.css',
-		'/db'					: '/db.js'
+		'/' 						 : '/home.html',
+		'/db'						 : '/db.js',
+		'/toDo' 		  	 : toDo + '/toDo.html',
+		'/toDoJs' 			 : toDo + scripts + '/toDo.js',
+		'/draggableJs'	 : toDo + scripts + '/draggable.js',
+		'/clientLists'	 : toDo + scripts + '/clientLists.js',
+		'/onLoad'			 	 : toDo + scripts + '/onLoad.js',
+		'/stylesheet'		 : toDo + styles + '/stylesheet.css',
+		'/responsive' 	 : toDo + styles + '/responsive.css',
+		'/portfolio'		 : portfolio + '/test.html',
+		'/port/css/main' : portfolio + '/css/main.css',
 	};
 	var makeRoute = function(route, file){
 		app.get(route, function (req, res) {
