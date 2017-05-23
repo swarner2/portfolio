d9348ae152e045ea750ebf917db4312da7a41326
Samuel Warner <samuelrwarner1@gmail.com>
2017-05-23 14:03:54 -0400

    changed chord seperators

--- songIntervals/songSheetDir.js
+++ songIntervals/songSheetDir.js
@@ -7,7 +7,7 @@ app.directive('songSheetDir',function(){
       key: {{song.key}} <br>
       scale notes: {{song.scale.join(", ")}} <br>
       <ul ng-repeat="(section, info) in song.sections">
-        <li>{{section}}: {{info.chords.join(' ')}} <br>
+        <li>{{section}}: {{info.chords.join(' - ')}} <br>
         intervals: {{song.sectionIntervals(info.chords).join(' - ') }}<br>
         </li>
       </ul>
