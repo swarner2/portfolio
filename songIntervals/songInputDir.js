434254e674ee87504ee5e11edcc14fdb0026ffe2
Samuel Warner <samuelrwarner1@gmail.com>
2017-05-25 14:08:14 -0400

    changed stylesheet

--- songIntervals/songInputDir.js
+++ songIntervals/songInputDir.js
@@ -2,20 +2,20 @@ app.directive("songInputDir",function(){
   return {
     template:
     `
-      <div>
+      <div id="inputContainer">
         <h3>Input</h3>
-        Name: <input type="text" ng-model="song.name" ng-change="changeTitle()"> <br>
-        Key: <input type="text" ng-model="song.key" ng-change=updateScale()>
+        <span class="inputText">Name: </span> <input type="text" ng-model="song.name" ng-change="changeTitle()"> <br>
+      <span class="inputText">  Key:</span>  <input type="text" ng-model="song.key" ng-change=updateScale()>
         <input type="Button" value={{song.majorMinor}} ng-click="toggleMajorMinor()">
         <br>
         <span ng-repeat="note in notes">
           <input type="button" value={{note}} ng-click="song.key = note; updateScale()">
         </span> <br>
-        Section Name: <input type="text" ng-model="newSectionName"> <br>
+      <span class="inputText">  Section Name: </span> <input type="text" ng-model="newSectionName"> <br>
         <span ng-repeat="sectionType in sectionTypes">
           <input type="button" value={{sectionType}} ng-click="addToNewSectionName(sectionType)">
         </span> <br>
-        Section Chords: <input type="text" ng-model="newSectionChords"> <br>
+      <span class="inputText">  Section Chords:</span>  <input type="text" ng-model="newSectionChords"> <br>
         <span ng-repeat="chord in song.scale">
           <input type="button" value={{chord}} ng-click="addToNewSectionChords(chord)">
         </span>
