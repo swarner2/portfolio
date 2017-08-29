96d71dbb75206a65969d1f68d7c9a5e893b8a75d
swarner2 <samuelrwarner1@gmail.com>
2017-08-29 12:54:12 -0400

    fixed same card click bug

--- matching_game/services/flipCardService.js
+++ matching_game/services/flipCardService.js
@@ -10,23 +10,23 @@ app.service('flipCardService', ['gameService',function(gameService){
       gameService.turn = 'Red'
     }
   }
+
   this.flipCard = function(card, index){
     if (this.clicked.length === 2) {
         this.clicked[0].clicked = false;
         this.clicked[1].clicked = false;
         this.clicked = [];
-        console.log('turn: ' + gameService.turn);
     }
-
     this.clicked.push(card)
     card.clicked = true;
-
+    if (this.clicked[0] === this.clicked[1]){
+      this.clicked.length = 1;
+    }
     if (this.clicked.length === 2) {
-      if (this.clicked[0].color == this.clicked[1].color  && this.clicked[0].number == this.clicked[1].number) {
-        console.log('point given');
-        gameService.score[this.turn]++
-        this.clicked = []
-      }
+        if (this.clicked[0].color == this.clicked[1].color  && this.clicked[0].number == this.clicked[1].number) {
+          gameService.score[this.turn]++
+          this.clicked = []
+        }
       changeTurn();
       this.turn = gameService.turn
     }
